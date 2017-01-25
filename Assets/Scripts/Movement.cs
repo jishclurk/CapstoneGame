629d475d530a8efab6e0375fd60430a98c339b34
BArthur789 <arthur.199@osu.edu>
2017-01-24 19:20:54 -0500

    Added basic code for exit state and exit state scene
    Only significant changes are the exit behavior added to movement script

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    // Use this for initialization
+    private float exitTime = 3.5f;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Translate (0.0f, -0.25f, 0.0f);
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate (0.0f, 0.25f, 0.0f);
+		}
+
+		if (transform.position.y > 7.5f) {
+			transform.position = new Vector3 (15.0f, 7.5f, 0.0f);
+		}
+		if (transform.position.y < -7.5f) {
+			transform.position = new Vector3 (15.0f, -7.5f, 0.0f);
+		}
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            //Switch to exit state scene before running below
+            while (exitTime > 0)
+            {
+                exitTime -= Time.deltaTime;
+            }
+                Application.Quit();
+            
+        }
+
+
+	}
+}
