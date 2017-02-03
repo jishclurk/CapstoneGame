386e75bce3b3136e41c8eeb3d439d556ccb4f6ee
curtonick <curtonick@gmail.com>
2017-02-02 23:53:01 -0500

    Finished NavMesh Tut

---
+++ Assets/NickTestStuff/OffsetCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffsetCamera : MonoBehaviour
+{
+
+    private GameObject player;
+    private Vector3 offset;
+
+    //Simple script from Roll a Ball
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/NickTestStuff/PlayerController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float shootDistance = 2.0f;
+
+
+    private Animator anim;
+    private NavMeshAgent navMeshAgent;
+    private Transform targetedEnemy;
+    private Ray shootRay;
+    private RaycastHit shootHit;
+    private bool walking;
+    private bool enemyClicked;
+    private float nextFire;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Input.GetButtonDown("Fire2"))
+        {
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.collider.CompareTag("Enemy"))
+                {
+                    targetedEnemy = hit.transform;
+                    enemyClicked = true;
+                }
+                else
+                {
+                    walking = true;
+                    enemyClicked = false;
+                    navMeshAgent.destination = hit.point;
+                    transform.LookAt(hit.point); //prevents slow turn, could be omitted or replaced with animation
+                    navMeshAgent.Resume();
+                }
+            }
+        }
+
+        if (enemyClicked)
+        {
+            MoveAndShoot();
+        }else
+        {
+            walking = navMeshAgent.remainingDistance > navMeshAgent.stoppingDistance;
+        }
+
+        anim.SetBool("Idling", !walking);
+        anim.SetBool("NonCombat", !enemyClicked);
+    }
+
+    private void MoveAndShoot()
+    {
+
+        if(targetedEnemy == null)
+        {
+            //this return happens if enemy dies
+            return; //avoid running code we don't need to.
+        }
+        else
+        {
+            navMeshAgent.destination = targetedEnemy.position;
+            if(navMeshAgent.remainingDistance >= shootDistance)
+            {
+                navMeshAgent.Resume();
+                walking = true;
+            }else
+            {
+                //Within range, look at enemy and shoot
+                transform.LookAt(targetedEnemy);
+                Vector3 dirToShoot = targetedEnemy.transform.position - transform.position;
+                Debug.Log("Shooting!"); //Call the shoot()/use() method of weapon or ability
+                navMeshAgent.Stop(); //within range, stop moving
+                walking = false;
+            }
+           
+
+        }
+    }
+}
