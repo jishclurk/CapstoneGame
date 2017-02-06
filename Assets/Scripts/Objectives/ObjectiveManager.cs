508bca56b8d3d69db38f257534c898e915724818
petrilla10 <petrilla.10@gmail.com>
2017-02-06 17:55:40 -0500

    UI prefabs added - nothing linked yet

---
+++ Assets/Scripts/Objectives/ObjectiveManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectiveManager : MonoBehaviour {
+
+	public List<GoalManager> objectives;
+
+	public Text goalListText;
+	private bool activeObjective;
+	private string objectiveList;
+	private int activeObjNum = 0;
+
+	void Awake() {
+		objectives = new List<GoalManager>(GetComponentsInChildren<GoalManager> ());
+		activeObjective = false;
+		objectiveList =  "Objectives: \n";
+	}
+
+	void Update() {
+		objectiveList =  "Objectives: \n";
+		for (int i = 0; i<objectives.Count ; i++){
+			objectiveList += "\n"+(i+1)+".  " + objectives[i].objectiveName;
+			if (!objectives [i].isComplete () && !activeObjective) {
+				activeObjective = true;
+				activeObjNum = i;
+			} 
+			if (!objectives [i].isComplete () && activeObjNum == i) {
+				objectiveList += "\n" + objectives [i].goalList () +"\n";
+			}
+			if (objectives [i].isComplete () && activeObjective) {
+				activeObjective = false;
+			}
+		}
+		goalListText.text = objectiveList;
+	}
+}
+
+
