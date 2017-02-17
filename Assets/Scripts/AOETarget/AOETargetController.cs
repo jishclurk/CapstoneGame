f5dccb7f32855eb3037ce0307a7bdc672d48b0e8
Bryan Arnold <bryanhallarnold@gmail.com>
2017-02-17 13:12:50 -0500

    Added AOETarget to Tb3
    Added AOETarget and AOETargetController.

---
+++ Assets/Scripts/AOETarget/AOETargetController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AOETargetController : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            if (hit.collider.CompareTag("Floor"))
+            {
+                transform.position = hit.point;
+            }
+        }
+    }
+}
