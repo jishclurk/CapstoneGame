c8701a1c7f895f84e053eb7b4dab91b852df24b9
Bryan Arnold <bryanhallarnold@gmail.com>
2017-03-02 20:26:24 -0500

    Fixed Main Camera movement, Mouse Scrollwheel moves camera vertically
    The camera doesn't "jerk" now. I got rid of the LookAt() method calls.
    The Mouse Scrollwheel now can move the camera up or down.

--- Assets/Scripts/OffsetCamera.cs
+++ Assets/Scripts/OffsetCamera.cs
@@ -11,6 +11,7 @@ public class OffsetCamera : MonoBehaviour
     private bool followPlayer;
     private float radius;
     private float offsetYValue;
+    private Vector3 fixedEulerAngles;
 
 
     //Simple script from Roll a Ball
@@ -23,6 +24,7 @@ public class OffsetCamera : MonoBehaviour
         followPlayer = false;
         radius = Mathf.Sqrt(offset.x * offset.x + offset.z * offset.z);
         offsetYValue = offset.y;
+        fixedEulerAngles = transform.eulerAngles;
     }
 
     void LateUpdate()
@@ -30,26 +32,59 @@ public class OffsetCamera : MonoBehaviour
         if (Input.GetKey(KeyCode.Minus) && offsetYValue > 6f)
         {
             offsetYValue -= 0.2f;
+            if (offsetYValue > 12f)
+            {
+                offsetYValue = 12f;
+            }
+            else if (offsetYValue < 6f)
+            {
+                offsetYValue = 6f;
+            }
             offset = new Vector3(offset.x, offsetYValue, offset.z);
         }
         if (Input.GetKey(KeyCode.Equals) && offsetYValue < 12f)
         {
+            if (offsetYValue > 12f)
+            {
+                offsetYValue = 12f;
+            }
+            else if (offsetYValue < 6f)
+            {
+                offsetYValue = 6f;
+            }
             offsetYValue += 0.2f;
             offset = new Vector3(offset.x, offsetYValue, offset.z);
         }
-        if(offsetYValue > 12f)
+        
+        if (Input.GetAxis("Mouse ScrollWheel") != 0 && offsetYValue >= 6f && offsetYValue <= 12f)
         {
-            offsetYValue = 12f;
-        }
-        else if(offsetYValue < 6f)
-        {
-            offsetYValue = 6f;
+            offsetYValue -= Input.GetAxis("Mouse ScrollWheel");
+            float xRotation = transform.eulerAngles.x - Input.GetAxis("Mouse ScrollWheel");
+            if(xRotation < 40f)
+            {
+                xRotation = 40f;
+            }
+            else if(xRotation > 50f)
+            {
+                xRotation = 50f;
+            }
+            if (offsetYValue > 12f)
+            {
+                offsetYValue = 12f;
+            }
+            else if (offsetYValue < 6f)
+            {
+                offsetYValue = 6f;
+            }
+            transform.eulerAngles = new Vector3(xRotation, transform.eulerAngles.y, transform.eulerAngles.z);
+            offset = new Vector3(offset.x, offsetYValue, offset.z);
         }
         if (Input.GetKeyDown(KeyCode.F))
         {
             if (followPlayer)
             {
                 followPlayer = false;
+                transform.eulerAngles = fixedEulerAngles;
             }
             else
             {
@@ -61,6 +96,7 @@ public class OffsetCamera : MonoBehaviour
             float angle = Mathf.Deg2Rad * activePlayerCharacter.transform.eulerAngles.y - Mathf.PI;
             Vector3 newPosition = new Vector3(activePlayerCharacter.transform.position.x + (radius * Mathf.Sin(angle)), offsetYValue, activePlayerCharacter.transform.position.z + (radius * Mathf.Cos(angle)));
             transform.position = Vector3.Lerp(transform.position, newPosition, 0.2f);
+            transform.LookAt(activePlayerCharacter.transform);
         }
         else
         {
@@ -73,7 +109,7 @@ public class OffsetCamera : MonoBehaviour
                 transform.position = activePlayerCharacter.transform.position + offset;
             }
         }
-        transform.LookAt(activePlayerCharacter.transform);
+        
     }
 
 }
