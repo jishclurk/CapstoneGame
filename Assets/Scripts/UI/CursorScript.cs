54fd322d427ac027efa8b42767d48a8b2e8c61d9
curtonick <curtonick@gmail.com>
2017-03-06 17:57:09 -0500

    Added custom cursor
    all is up for change. Still needs to work with abilities.

---
+++ Assets/Scripts/UI/CursorScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LayerDefinitions;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour {
+
+    public Texture2D navTexture;
+    public Texture2D shootTexture;
+    public Texture2D abilityTexture;
+    public CursorMode cursorMode = CursorMode.ForceSoftware;
+
+    // Use this for initialization
+    void Start () {
+        
+        Cursor.SetCursor(navTexture, Vector2.zero, cursorMode);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100f, Layers.NonWall))
+        {
+            if (hit.collider.CompareTag("Enemy"))
+            {
+                Cursor.SetCursor(shootTexture, new Vector2(shootTexture.height/2, shootTexture.width/2), cursorMode);
+            }
+            else
+            {
+                Cursor.SetCursor(navTexture, Vector2.zero, cursorMode);
+            }
+
+
+        }
+
+
+
+    }
+}
