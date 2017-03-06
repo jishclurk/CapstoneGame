76253f49dda75d70abaf987d48476ee5b810a9b3
petrilla10 <petrilla.10@gmail.com>
2017-03-06 00:02:12 -0500

    Changes to Puzzle Prefabs - added pop-up to generator/console

--- Assets/Scripts/Puzzles/Gen_Collide.cs
+++ Assets/Scripts/Puzzles/Gen_Collide.cs
@@ -6,22 +6,46 @@ public class Gen_Collide : MonoBehaviour {
 
 	public bool triggered;
 	private AudioSource sound;
+	GameObject popUpObject;
+	TeamManager tm;
+	bool active;
 	// Use this for initialization
 	void Start () {
 		triggered = false;
+		active = false;
 		sound = GetComponent<AudioSource>();
+		tm = FindObjectOfType<TeamManager> ();
+		popUpObject = transform.FindChild ("PopupText").gameObject;
+		popUpObject.gameObject.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (active) {
+			if (Input.GetKeyDown(KeyCode.M)){
+				sound.Play ();
+				Debug.Log ("Generator flipped");
+				triggered = !triggered;
+			}
+
+		}
 	}
 
 	void OnTriggerEnter(Collider other){
-		if ((other.gameObject.tag == "Player") && (!other.isTrigger)) {
-				sound.Play();
-				Debug.Log ("Generator flipped");
-				triggered = !triggered;
+		if ((other.gameObject == tm.activePlayer.gameObject) && (!other.isTrigger)) {
+			popUpObject.gameObject.SetActive (true);
+			active = true;
+
+		} else {
+			popUpObject.gameObject.SetActive (false);
+			active = false;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if ((other.gameObject == tm.activePlayer.gameObject) && (!other.isTrigger)) {
+			popUpObject.gameObject.SetActive (false);
+					active = false;
 		}
 	}
 		
---
+++ Assets/Scripts/Puzzles/PopupText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupText : MonoBehaviour {
+
+	// Use this for initialization
+	Camera cam;
+	void Start () {
+		cam = GetComponent<Canvas> ().rootCanvas.worldCamera;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 v = cam.transform.position - transform.position;
+		v.x = v.z = 0.0f;
+		transform.LookAt( cam.transform.position - v ); 
+		transform.Rotate(0,180,0);
+	}
+}
