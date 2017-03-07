fca8f2ca1f1f29fedc64cae083084d22e98b74db
Bryan Arnold <bryanhallarnold@gmail.com>
2017-03-07 18:29:42 -0500

    Added InputBinding.cs
    InputBinding.cs contains static variables for our KeyCode bindings.

---
+++ Assets/Scripts/Input/InputBinding.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputBinding
+{
+
+    public static KeyCode selectPlayerCharacter1 = KeyCode.Alpha1;
+    public static KeyCode selectPlayerCharacter2 = KeyCode.Alpha2;
+    public static KeyCode selectPlayerCharacter3 = KeyCode.Alpha3;
+    public static KeyCode selectPlayerCharacter4 = KeyCode.Alpha4;
+    public static KeyCode cyclePlayerCharacter = KeyCode.Tab;
+    public static KeyCode specialAbility1 = KeyCode.Q;
+    public static KeyCode specialAbility2 = KeyCode.W;
+    public static KeyCode specialAbility3 = KeyCode.E;
+    public static KeyCode specialAbility4 = KeyCode.R;
+    public static KeyCode tacticalPause = KeyCode.Space;
+    public static KeyCode characterAbilityScreen = KeyCode.C;
+    public static KeyCode defaultAbilities = KeyCode.P;
+    public static KeyCode hurtTeamBy10 = KeyCode.H;
+    public static KeyCode lowerMainCamera = KeyCode.Minus;
+    public static KeyCode raiseMainCamera = KeyCode.Plus;
+    public static KeyCode mainCameraFollow = KeyCode.F;
+
+}
