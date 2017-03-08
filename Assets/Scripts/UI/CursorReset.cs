fc833f16143b72614cedbbe7d4474393ed281c5b
curtonick <curtonick@gmail.com>
2017-03-07 23:56:35 -0500

    whoops I left this unchecked

---
+++ Assets/Scripts/UI/CursorReset.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorReset : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+	}
+	
+}
