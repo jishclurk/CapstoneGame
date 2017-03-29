126efc8e054d48611fecf00240fdc0cf60d06c04
Josh Clark <clark.2525@osu.edu>
2017-03-28 21:57:26 -0400

    Added cooldown UI/scripts

---
+++ Assets/Scripts/UI/CooldownTimerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CooldownTimerScript : MonoBehaviour {
+
+    public Image Overlay;
+    public Text Timer;
+    public int index = 0;
+    private TeamManager tm;
+
+    void Awake ()
+    {
+        tm = GameObject.FindWithTag("TeamManager").GetComponent<TeamManager>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        ISpecial abil = tm.activePlayer.abilities.abilityArray[index];
+        if (abil != null)
+        {
+            bool visible = abil.isReady();
+            Overlay.gameObject.SetActive(visible);
+            Timer.gameObject.SetActive(visible);
+            if (visible)
+            {
+                float timeLeft = abil.RemainingTime();
+                float totalTime = abil.coolDownTime;
+                Overlay.fillAmount = timeLeft / totalTime;
+                Timer.text = ((int)timeLeft).ToString();
+            }
+        }
+        
+	}
+}
