329f57e984bb90fe210c9e12819498f275258e8b
petrilla10 <petrilla.10@gmail.com>
2017-04-03 10:34:51 -0400

    Clock puzzle added to Level 2

---
+++ Assets/Scripts/Puzzles/ClockPuzzle.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockPuzzle : MonoBehaviour {
+
+	bool btest;
+	int hour;
+	int min;
+	Light genSpot;
+	bool turnedOn;
+	HourScript hourPart;
+	MinuteScript minutePart;
+	// Use this for initialization
+	void Start () {
+		hour = System.DateTime.Now.Hour;
+		min = System.DateTime.Now.Minute;
+		btest = true;
+		genSpot = this.gameObject.transform.Find ("Spot Light").GetComponent<Light> ();
+		turnedOn = false;
+		hourPart = this.transform.FindChild ("Hours").GetComponent<HourScript> ();
+		minutePart = this.transform.FindChild ("Minutes").GetComponent<MinuteScript> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		hour = System.DateTime.Now.Hour % 12;
+		min = System.DateTime.Now.Minute;
+		if (hour == 0) {
+			hour = 12;
+		}
+		if (btest) {
+			print ("\nHour: " + hour+"\nMinute: "+min);
+			btest = false;
+		}
+		hourPart.setActiveButton(hour);
+		minutePart.setActiveButton (min);
+		if (hourPart.Complete() && minutePart.Complete()) {
+			print ("Clock Accomplished");
+			genSpot.intensity = 100;
+
+		} else {
+			genSpot.intensity = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/Puzzles/MinuteScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinuteScript : MonoBehaviour {
+
+	int r =12;
+	public Minute[] minuteButtons;
+	public Minute minuteButton;
+	int incpressed;
+	int activeNum;
+	bool pressed;
+	// Use this for initialization
+	void Start () {
+		minuteButtons = new Minute[60];
+		for (int i =0; i<60;i++){
+			minuteButtons[i] = (Minute) Instantiate (minuteButton, new Vector3 (transform.parent.transform.position.x + 
+				r*Mathf.Cos(-Mathf.PI*2*i/60), transform.parent.transform.position.y, 
+				transform.parent.transform.position.z+ r*Mathf.Sin(-Mathf.PI*2*i/60)), Quaternion.identity);
+			minuteButtons[i].transform.parent = gameObject.transform;
+			minuteButtons [i].setTime (i);
+			//minuteButton.transform.parent = gameObject.transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public int activeButton(){
+
+		return activeNum;
+
+	}
+
+	public void setActiveButton(int i){
+		activeNum = i;
+	}
+
+	public void incrementActive(){
+		incpressed++;
+	}
+
+	public void decrementActive(){
+		incpressed--;
+	}
+
+	public void activePressed(bool p){
+
+		pressed = p;
+	}
+
+	public bool Complete(){
+		return (pressed && incpressed == 1);
+	}
+}
