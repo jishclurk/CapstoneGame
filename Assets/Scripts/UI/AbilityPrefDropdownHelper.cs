87d59af96581354f2caa3f7bd203a0f3dba297d7
Josh Clark <clark.2525@osu.edu>
2017-04-05 23:46:42 -0400

    Backin up my prefab

---
+++ Assets/Scripts/UI/AbilityPrefDropdownHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityPrefDropdownHelper : MonoBehaviour {
+
+    public Dropdown abilityDropdown;
+    public Dropdown targetDropdown;
+
+    void Start () {
+        abilityDropdown.onValueChanged.AddListener(delegate {
+            abilityValueChangedHandler(abilityDropdown);
+        });
+    }
+
+    void Destroy()
+    {
+        abilityDropdown.onValueChanged.RemoveAllListeners();
+        targetDropdown.onValueChanged.RemoveAllListeners();
+    }
+
+    private void abilityValueChangedHandler(Dropdown target)
+    {
+        
+    }
+
+    private void targetValueChangedHandler(Dropdown target)
+    {
+
+    }
+}
