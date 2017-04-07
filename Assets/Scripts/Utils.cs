3c2c6924ca942bb8e755508cfff1b9208e986faf
curtonick <curtonick@gmail.com>
2017-04-06 23:32:27 -0400

    fixed the wrench

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -10,7 +10,7 @@ public static class Utils  {
     {
         {1,  new RifleShot()}, {2, new SMGShot()}, {3, new ShotgunShot() }, {4, new SniperShot() }, {5, new BioGrenade() },
         { 6, new ChainLightning() }, {7, new Flamethrower() }, {8, new FlameWall() }, {9, new GrenadeThrow() }, {10, new Invigorate() },
-        { 11, new LeechDart() }, {12, new MedKit() }, {13, new Revive() }, {14, new RIPRounds() }, {15, new Shockwave() },
+        { 11, new LeechDart() }, {12, new MedKit() }, {13, new Revive() }, {14, new RIPRounds() }, {15, new SentryTurret() },
         { 16, new ShieldBooster() }, {17, new Shockwave() }, {18, new Stimpak() }, {19, new Zap() }
     };
 
