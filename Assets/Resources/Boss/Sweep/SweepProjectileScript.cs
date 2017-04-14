b3f26850b6f46d180fdabfbab58e43595e33e3f4
Josh Clark <clark.2525@osu.edu>
2017-04-13 20:47:25 -0400

    First two boss abilities implemented

---
+++ Assets/Resources/Boss/Sweep/SweepProjectileScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SweepProjectileScript : MonoBehaviour {
+
+    public GameObject impactParticle;
+    public Vector3 destination;
+    public float damage;
+    public GameObject projectileParticle;
+    [HideInInspector]
+    public Vector3 impactNormal; //Used to rotate impactparticle.
+    public float rot;
+    public float velocityMultiplier = 10.0f;
+
+    private bool hasCollided = false;
+
+    void Start()
+    {
+        transform.LookAt(destination);
+        transform.Rotate(0.0f, rot, 0.0f);
+        projectileParticle = Instantiate(projectileParticle, transform.position, transform.rotation) as GameObject;
+        projectileParticle.transform.parent = transform;
+        Vector3 normalizedTrajectory = transform.forward;
+        GetComponent<Rigidbody>().velocity = normalizedTrajectory * velocityMultiplier;
+        Destroy(gameObject, 5f);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!hasCollided && other.CompareTag("Player") && !other.isTrigger && !other.GetComponent<PlayerResources>().isDead)
+        {
+            hasCollided = true;
+            impactParticle = Instantiate(impactParticle, transform.position, Quaternion.FromToRotation(Vector3.up, impactNormal)) as GameObject;
+
+            other.GetComponent<PlayerResources>().TakeDamage(damage);
+
+            Destroy(projectileParticle, 3f);
+            Destroy(impactParticle, 5f);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/Boss/BossAttackExecutor.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAttackExecutor : MonoBehaviour {
+
+    public Transform SweepProjectileOrigin;
+    public float SweepProjectileDamage = 15f;
+    public float ExplosionRadius = 6f;
+    public float ExplosionDamage = 15f;
+
+    public Transform tempTarget;
+    private Object sweepProj;
+    private Object explostionEffect;
+    private TeamManager tm;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        tm = GameObject.FindWithTag("TeamManager").GetComponent<TeamManager>();
+        sweepProj = Resources.Load("Boss/Sweep/SweepProjectile");
+        explostionEffect = Resources.Load("Boss/Explosion/ExplosionEffect");
+    }
+	
+	
+    public void ExecuteSweepAttack()
+    {
+        Vector3 targetDest = new Vector3(tempTarget.transform.position.x, SweepProjectileOrigin.position.y, tempTarget.transform.position.z);
+
+        for (float shellRot = -30.0f; shellRot < 30.1f; shellRot += 20.0f)
+        {
+            GameObject project = Instantiate(sweepProj, SweepProjectileOrigin.position, Quaternion.identity) as GameObject;
+            project.GetComponent<SweepProjectileScript>().destination = targetDest;
+            project.GetComponent<SweepProjectileScript>().rot = shellRot;
+            project.GetComponent<SweepProjectileScript>().damage = SweepProjectileDamage;
+        }
+    }
+
+    public void ExecuteExplosionAttack()
+    {
+        GameObject explosion = Instantiate(explostionEffect, 
+                                           new Vector3(this.transform.position.x, this.transform.position.y + 0.5f, this.transform.position.z), 
+                                           Quaternion.Euler(90, 0, 0)) as GameObject;
+
+        foreach (Player player in tm.playerList) 
+        {
+            PlayerResources resources = player.GetComponent<PlayerResources>();
+            if ((Mathf.Abs(Vector3.Distance(this.transform.position, player.transform.position)) < ExplosionRadius) && !resources.isDead)
+            {
+                resources.TakeDamage(ExplosionDamage);
+            }
+        }
+    }
+
+    public void StartLaserAttack()
+    {
+
+    }
+
+    public void EndLaserAttack()
+    {
+
+    }
+
+    public void ExecuteSpawnAttack()
+    {
+
+    }
+}
