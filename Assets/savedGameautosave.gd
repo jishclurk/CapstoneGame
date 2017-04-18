00774f5a5b4c655bbb9492ce0eeb64e502c69821
BArthur789 <arthur.199@osu.edu>
2017-04-18 03:19:28 -0400

    Added new green player material

--- Assets/savedGameautosave.gd
+++ Assets/savedGameautosave.gd
@@ -2,32 +2,32 @@
 <GameState xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
   <name>autosave</name>
   <Level>1</Level>
-  <Check_x0020_Point>1</Check_x0020_Point>
+  <Check_x0020_Point>0</Check_x0020_Point>
   <Players>
     <Player>
       <In_x0020_Control>true</In_x0020_Control>
       <level>1</level>
-      <experience>7</experience>
-      <statPoints>0</statPoints>
-      <strength>11</strength>
-      <intelligence>6</intelligence>
+      <experience>0</experience>
+      <statPoints>5</statPoints>
+      <strength>1</strength>
+      <intelligence>1</intelligence>
       <stamina>1</stamina>
       <id>1</id>
       <health>100</health>
       <energy>100</energy>
       <Abilities>
-        <Ability>9</Ability>
-        <Ability>12</Ability>
-        <Ability>11</Ability>
-        <Ability>7</Ability>
+        <Ability>0</Ability>
+        <Ability>0</Ability>
+        <Ability>0</Ability>
+        <Ability>0</Ability>
       </Abilities>
       <basic>1</basic>
     </Player>
     <Player>
       <In_x0020_Control>false</In_x0020_Control>
       <level>1</level>
-      <experience>7</experience>
-      <statPoints>15</statPoints>
+      <experience>0</experience>
+      <statPoints>5</statPoints>
       <strength>1</strength>
       <intelligence>1</intelligence>
       <stamina>1</stamina>
@@ -45,8 +45,8 @@
     <Player>
       <In_x0020_Control>false</In_x0020_Control>
       <level>1</level>
-      <experience>7</experience>
-      <statPoints>15</statPoints>
+      <experience>0</experience>
+      <statPoints>5</statPoints>
       <strength>1</strength>
       <intelligence>1</intelligence>
       <stamina>1</stamina>
@@ -64,8 +64,8 @@
     <Player>
       <In_x0020_Control>false</In_x0020_Control>
       <level>1</level>
-      <experience>7</experience>
-      <statPoints>15</statPoints>
+      <experience>0</experience>
+      <statPoints>5</statPoints>
       <strength>1</strength>
       <intelligence>1</intelligence>
       <stamina>1</stamina>
