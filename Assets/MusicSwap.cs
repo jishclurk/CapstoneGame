17d3c723e3a4fd2f88daa9f630880be01603f0fe
BArthur789 <arthur.199@osu.edu>
2017-04-18 21:57:57 -0400

    Added music swapping for boss fight

---
+++ Assets/MusicSwap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSwap : MonoBehaviour {
+
+    // Use this for initialization
+    private AudioSource thisPlayer;
+    private bool isTriggered;
+    void Start () {
+        thisPlayer = GetComponent<AudioSource>();
+        isTriggered = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+    if (other.gameObject.CompareTag("Player") && !isTriggered)
+        {
+            isTriggered = true;
+            thisPlayer.Play();
+        }
+    }
+}
